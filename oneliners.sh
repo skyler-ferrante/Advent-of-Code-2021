cd7c5aabad7953a5048d205a7751e487f141aaa7
skyler-ferrante <sjf5462@rit.edu>
2021-12-01 12:37:57 -0500

    Made it a lot smaller
    Uses xargs with no arguement piped to awk

--- oneliners.sh
+++ oneliners.sh
@@ -1,7 +1,7 @@
 # These are pretty long, but I still consider them oneliners, since I wrote them without an IDE
 
 # Day one, star 1
-cat 1/input.txt | awk 'BEGIN { RS = ""; OFS = "\n"; } { for(i=1; i<=NF; i++) if($i > $(i-1)) print "I" }' | wc -l
+cat 1/input.txt | xargs | awk '{for(i=1; i<NF+1; i++) if($i>$(i-1)) print ""}' | wc -l
 
 # Day one, star 2
-cat 1/input.txt | awk 'BEGIN {RS = ""; OFS = "\n"; } { for(i=4; i<=NF; i++) if($i+$(i-1)+$(i-2) > $(i-1)+$(i-2)+$(i-3)) print "I"; }' | wc -l
+cat 1/input.txt | xargs | awk '{for(i=4; i<=NF+1; i++) if($i+$(i-1)+$(i-2) > $(i-1)+$(i-2)+$(i-3)) print "I"; }' | wc -l
