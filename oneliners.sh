6a668cc999a43b027321c27173c2f5d07d635855
skyler-ferrante <sjf5462@rit.edu>
2021-12-01 01:25:09 -0500

    Removed unneeded newline
    Day1 Star2 Oneliners

--- oneliners.sh
+++ oneliners.sh
@@ -4,5 +4,4 @@
 cat 1/input.txt | awk 'BEGIN { RS = ""; OFS = "\n"; ORS = "\n\n" } {for(i=1; i<=NF; i++) if($i > $(i-1)) print "INC" }' | grep "INC" | wc -l
 
 # Day one, star 2
-cat 1/input.txt | awk 'BEGIN { RS = ""; OFS = "\n"; ORS = "\n\n" } {for(i=4; i<=NF; i+
-+) if($i+$(i-1)+$(i-2) > $(i-1)+$(i-2)+$(i-3)) print "INC" }' | grep "INC" | wc -l
+cat 1/input.txt | awk 'BEGIN { RS = ""; OFS = "\n"; ORS = "\n\n" } {for(i=4; i<=NF; i++) if($i+$(i-1)+$(i-2) > $(i-1)+$(i-2)+$(i-3)) print "INC" }' | grep "INC" | wc -l
