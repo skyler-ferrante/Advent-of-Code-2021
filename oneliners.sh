cfda6d0c0f6c868501c9747cdeb28fb66ed32e4b
skyler-ferrante <sjf5462@rit.edu>
2021-12-01 01:23:49 -0500

    Added oneliner bash solution for Day1 star 2

--- oneliners.sh
+++ oneliners.sh
@@ -1,2 +1,8 @@
-#Day one
+# These are pretty long, but I still consider them oneliners, since I wrote them without an IDE
+
+# Day one, star 1
 cat 1/input.txt | awk 'BEGIN { RS = ""; OFS = "\n"; ORS = "\n\n" } {for(i=1; i<=NF; i++) if($i > $(i-1)) print "INC" }' | grep "INC" | wc -l
+
+# Day one, star 2
+cat 1/input.txt | awk 'BEGIN { RS = ""; OFS = "\n"; ORS = "\n\n" } {for(i=4; i<=NF; i+
++) if($i+$(i-1)+$(i-2) > $(i-1)+$(i-2)+$(i-3)) print "INC" }' | grep "INC" | wc -l
