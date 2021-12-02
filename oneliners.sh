5945f4112c63f4c8d00568a1fc5636d3b0b0662b
skyler-ferrante <sjf5462@rit.edu>
2021-12-02 00:21:59 -0500

    Added solution for Day2 star1 in oneliners

--- oneliners.sh
+++ oneliners.sh
@@ -1,7 +1,10 @@
 # These are pretty long, but I still consider them oneliners, since I wrote them without an IDE
 
-# Day one, star 1
+echo -n "Day one, star1:  "
 cat 1/input.txt|awk 'BEGIN{l=$1} {if(NR>1) if($i>l) print$i; l=$1}'|wc -l
 
-# Day one, star 2
+echo -n "Day one, star2:  " 
 cat 1/input.txt|xargs|awk '{for(i=4;i<=NF+1;i++)if($i+$(i-1)+$(i-2)>$(i-1)+$(i-2)+$(i-3))print""}'|wc -l
+
+echo -n "Day two, star1:  "
+cat 2/input.txt | awk 'BEGIN{h=0;d=0}{if($1=="forward"){h+=$2}else if($1=="up"){d-=$2}else d+=$2} END{print d*h}'
