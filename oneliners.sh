a09080435933b5eaad10a9923ca41ef5856a86be
skyler-ferrante <sjf5462@rit.edu>
2021-12-02 00:31:09 -0500

    Day2, star2
    I made Day2 star1 shorter as well

--- oneliners.sh
+++ oneliners.sh
@@ -6,5 +6,10 @@ cat 1/input.txt|awk 'BEGIN{l=$1} {if(NR>1) if($i>l) print$i; l=$1}'|wc -l
 echo -n "Day one, star2:  " 
 cat 1/input.txt|xargs|awk '{for(i=4;i<=NF+1;i++)if($i+$(i-1)+$(i-2)>$(i-1)+$(i-2)+$(i-3))print""}'|wc -l
 
+echo
+
 echo -n "Day two, star1:  "
-cat 2/input.txt | awk 'BEGIN{h=0;d=0}{if($1=="forward"){h+=$2}else if($1=="up"){d-=$2}else d+=$2} END{print d*h}'
+cat 2/input.txt | awk 'BEGIN{h=0;d=0}{if($1=="down"){d+=$2}else if($1=="up"){d-=$2}else h+=$2}END{print d*h}'
+
+echo -n "Day two, star2:  "
+cat 2/input.txt | awk 'BEGIN{h=0;d=0;a=0}{if($1=="down"){a+=$2}else if($1=="up"){a-=$2}else{d+=$2;h+=$2*a}}END{print d*h}'
